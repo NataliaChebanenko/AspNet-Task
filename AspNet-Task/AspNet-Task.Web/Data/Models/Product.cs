aa65e02d3cf46d373f279ea0ac693d0871f786d6
NataliaChebanenko <111647452+NataliaChebanenko@users.noreply.github.com>
2023-05-27 16:24:34 +0200

    Add database structure
    Added Product and Category tables to hold data and also the context for communication purposes.

---
+++ AspNet-Task/AspNet-Task.Web/Data/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace AspNet_Task.Web.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string SKU { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ AspNet-Task/AspNet-Task.Web/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNet_Task.Web.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNet_Task.Web.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+        }
