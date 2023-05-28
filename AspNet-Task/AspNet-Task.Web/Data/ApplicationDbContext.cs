9e82041ce36d0d2b9fccb8c7478c62a50eb1d587
NataliaChebanenko <111647452+NataliaChebanenko@users.noreply.github.com>
2023-05-28 13:12:22 +0200

    Add contact table
    Added table to store the contact entry form from the user.

--- AspNet-Task/AspNet-Task.Web/Data/ApplicationDbContext.cs
+++ AspNet-Task/AspNet-Task.Web/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AspNet_Task.Web.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspNet_Task.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Contact> Contacts { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ AspNet-Task/AspNet-Task.Web/Data/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNet_Task.Web.Data.Models
+{
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}
