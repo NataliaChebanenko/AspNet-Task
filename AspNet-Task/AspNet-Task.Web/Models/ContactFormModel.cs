86852d35a6b6cabfac514509cd721b9762abae16
NataliaChebanenko <111647452+NataliaChebanenko@users.noreply.github.com>
2023-05-28 13:13:52 +0200

    Add contact page
    Added a contact page to hold the partial contact view.

---
+++ AspNet-Task/AspNet-Task.Web/Models/ContactFormModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNet_Task.Web.Models
+{
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}
